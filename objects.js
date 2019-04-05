e4e3003c0f486750483163d1ae5b8dfb1d6ab6b0
Jowita Lapies <jowita.lapies@gmail.com>
2019-04-05 11:35:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
 var playlist = {Slowdive: 'Alison', MyBloodyValentine: 'Sometimes'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, playlist,  { artistName: songTitle });
+}
